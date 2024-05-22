b64ad3671a24cba821fc30f9d90af0acf09cdb7d
Aditya <me@xditya.me>
2024-05-22 15:07:49 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+import { Application, Router } from "oak";
+
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.redirect("https://xditya.me");
+});
+
+router.get("/light", (ctx) => {
+  ctx.response.body = "abc";
+});
+
+const app = new Application();
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  await next();
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("error", (e) => console.log(e));
+
+console.log("> Started listeneing on PORT 8000!");
+
+await app.listen({ port: 8000 });
